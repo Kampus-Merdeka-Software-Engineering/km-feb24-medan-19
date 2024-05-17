581da04fdc1456da08d801e66f861538bccf7f9e
Khaerul Azmi <khaerulazmi882@gmail.com>
2024-05-17 18:17:44 +0700

    Co-authored-by: Nipe Setiawan Harefa <nipeharefa@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,47 +24,78 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Populate recent orders list
   const recentOrdersContainer = document.querySelector('.recentOrders'); // Select the correct container
-  // fetch('assets/json/coffeeData.json')
-  //     .then(response => response.json())
-  //     .then(coffeeData => {
-  //         const ordersToDisplay = 5; // Jumlah pesanan terbaru yang ingin ditampilkan
-  //         coffeeData.recentOrders.slice(0, ordersToDisplay).forEach(order => {
-  //             const orderItem = document.createElement('div');
-  //             orderItem.classList.add('recentOrder');
-  //             orderItem.innerHTML = `
-  //                 <div class="imgBx"><img src="assets/img/profil.jpg" alt="${order.customer}"></div>
-  //                 <div class="order-info">
-  //                     <div class="name">${order.customer}</div>
-  //                     <div class="coffee">${order.coffee}</div>
-  //                 </div>
-  //                 <div class="price">$${order.price.toFixed(2)}</div>
-  //                 <div class="date">${order.orderDate}</div>
-  //                 <div class="status ${order.status.toLowerCase()}">${order.status}</div>`;
-  //             recentOrdersContainer.appendChild(orderItem); 
-  //         });
-  //     })
-  //     .catch(error => {
-  //         console.error('Error fetching data:', error); 
-  //         recentOrdersContainer.innerHTML = '<p>Failed to load recent orders.</p>'; 
-  //     });
-  const ff = async () => {
-      const response = await fetch('assets/json/data.json');
-      const reader = response.body.getReader();
-      const decoder = new TextDecoder();
-      
-      let json = '';
-      let done = false;
+  fetch('assets/json/data.json')
+      .then(response => response.json())
+      .then(coffeeData => {
+          // total revenue 
 
-      while (!done) {
-          const { value, done: readerDone } = await reader.read();
-          done = readerDone;
-          json += decoder.decode(value, { stream: true });
-      }
+          // console.log(coffeeData)
 
-      const data = JSON.parse(json);
+          // fungsi reduce wajib punya 2 parameter
+          // 1. fungsi proses
+          // 2. nilai awal
+          // 
+          const totalRevenue = coffeeData.reduce((ac, current) => {
+            // 0.18 + '1'
+            // 0.18 + 1
+            return ac + parseInt(current['Revenue']);
+          }, 0);
 
-      console.log(data)
-  };
+          const revenueNumber = document.querySelector('.revenue-number');
+          revenueNumber.textContent = totalRevenue;
 
-  ff();
+          // const productTypeObject = {
+          //   'category': 100,
+          //   ''
+          // };
+
+          const productTypeObject = coffeeData.reduce((ac, current) => {
+            
+            let c = ac[current.product_category] || 0;
+
+            c+=1
+            ac[current.product_category] = c;
+
+            return ac;
+          }, {});
+
+          const ctx1 = document.getElementById("chart-1").getContext("2d");
+          const myChart = new Chart(ctx1, {
+            type: "pie",
+            data: {
+              labels: Object.keys(productTypeObject),
+              datasets: [
+                {
+                  label: "# of Orders",
+                  data: Object.values(productTypeObject),
+                },
+              ],
+            },
+            options: {
+              responsive: true,
+            },
+          });
+
+          // console.log(Object.values(productTypeObject))
+
+          // const ordersToDisplay = 5; // Jumlah pesanan terbaru yang ingin ditampilkan
+          // coffeeData.recentOrders.slice(0, ordersToDisplay).forEach(order => {
+          //     const orderItem = document.createElement('div');
+          //     orderItem.classList.add('recentOrder');
+          //     orderItem.innerHTML = `
+          //         <div class="imgBx"><img src="assets/img/profil.jpg" alt="${order.customer}"></div>
+          //         <div class="order-info">
+          //             <div class="name">${order.customer}</div>
+          //             <div class="coffee">${order.coffee}</div>
+          //         </div>
+          //         <div class="price">$${order.price.toFixed(2)}</div>
+          //         <div class="date">${order.orderDate}</div>
+          //         <div class="status ${order.status.toLowerCase()}">${order.status}</div>`;
+          //     recentOrdersContainer.appendChild(orderItem); 
+          // });
+      })
+      .catch(error => {
+          console.error('Error fetching data:', error); 
+          recentOrdersContainer.innerHTML = '<p>Failed to load recent orders.</p>'; 
+      });
 });
