c99edb1b555ebb7b850947f36824caa2a3c4faf9
Novita Adelin Br Lumbantbing <129706316+novitaadelin@users.noreply.github.com>
2024-05-17 11:15:23 +0700

    Update chartsJS.js

--- assets/js/chartsJS.js
+++ assets/js/chartsJS.js
@@ -3,15 +3,17 @@ const ctx1 = document.getElementById("chart-1").getContext("2d");
 const myChart = new Chart(ctx1, {
   type: "pie",
   data: {
-    labels: ["aku", "kamu", "dia"],
+    labels: ["Cappuccino", "Latte", "Espresso", "Americano", "Tea"],
     datasets: [
       {
         label: "# of Orders",
-        data: [],
+        data: [30, 50, 20, 45, 15],
         backgroundColor: [
-          ,
-          ,
-          ,
+          "#AF8F6F",    // Cappuccino
+          "#74512D",    // Latte
+          "#543310",    //Espresso
+          "#151515",    //Americano
+          "#F8C794",    // Tea
         ],
       },
     ],
@@ -21,20 +23,23 @@ const myChart = new Chart(ctx1, {
   },
 });
 
+
 // Chart 2: Bar Chart
 const ctx2 = document.getElementById("chart-2").getContext("2d");
 const myChart2 = new Chart(ctx2, {
   type: "bar",
   data: {
-    labels: ["aku", "kamu", "dia"],
+    labels: ["Cappuccino", "Latte", "Espresso", "Americano", "Tea"],
     datasets: [
       {
         label: "Pendapatan",
-        data: [],
+        data: [1000000, 1500000, 6450000, 3520000, 2580000],
         backgroundColor: [
-          ,
-          ,
-          ,
+          "#AF8F6F",    // Cappuccino
+          "#74512D",    // Latte
+          "#543310",    //Espresso
+          "#151515",    //Americano
+          "#F8C794",    // Tea
         ],
       },
     ],
